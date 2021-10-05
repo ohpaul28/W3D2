8d00fe6853da8fda985766f452de618206ac2946
Paul <dustin.paul.oh@gmail.com>
2021-10-05 14:33:01 -0700

    05 solved

--- starter/problems/05-reverse.js
+++ starter/problems/05-reverse.js
@@ -4,15 +4,42 @@ it reversed.
 
 Examples:
 
-reverse("house"); // "esuoh"
-reverse("dog"); // "god"
-reverse("atom"); // "mota"
-reverse("q"); // "q"
-reverse("id"); // "di"
-reverse(""); // ""
 ***********************************************************************/
+let reverse = (string, newArr = []) => {
+  // let arr = string.split("");
+  // debugger;
+  if (!string.length) return newArr.join(""); // base case
+    // debugger;
+  /* } else {
+    newArr.push(arr.pop())
+  */
+  // debugger;
+  newArr.push(string.slice(string.length - 1))
 
-// your code here
+  return reverse(string.slice(0, string.length - 1), newArr) // recursive step
+
+}
+
+// let reverse = (string, newArr = []) => {
+
+//   let arr = string.split('');
+
+//   if (!string.length) return newArr.join('')
+
+//   newArr.push(arr.slice(arr.length - 1))
+
+//   return reverse(arr.slice(0, arr.length - 1), newArr)
+
+// }
+
+
+
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
