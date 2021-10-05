1fef23be5b9f2e4ce26e3c2e126046ff56be1686
Katerina Kreibich <87352324+Code-rina@users.noreply.github.com>
2021-10-05 10:51:14 -0700

    02 solved

--- starter/problems/02-sum-array.js
+++ starter/problems/02-sum-array.js
@@ -5,13 +5,28 @@ include a mix of positive and negative integers!
 
 Examples:
 
-sumArray([1, 2, 3]); //  6
-sumArray([0, 1, -3]); //  -2
-sumArray([1, 2, 3, 4, 5]); // 15
+
 ***********************************************************************/
 
-// your code here
+// let sumArray = arr => {
+//     if (!arr.length) return []; // base case
+    
+//     let ele = arr.pop(); //recursive case
+//     let sum = 0 + ele;
+
+//     return sum + sumArray(); //recursive step
+// }
+
+const sumArray = arr => {
+  if (!arr.length) return 0; // base case
+
+  return arr[0] + sumArray(arr.slice(1)); // recursive step
   
+}
+
+console.log(sumArray([1, 2, 3])); //  6
+console.log(sumArray([0, 1, -3])); //  -2
+console.log(sumArray([1, 2, 3, 4, 5])); // 15
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
