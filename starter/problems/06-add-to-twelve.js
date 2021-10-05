853632a95993b4f9c21dc4242a56e1367a093fc0
Katerina Kreibich <87352324+Code-rina@users.noreply.github.com>
2021-10-05 15:10:46 -0700

    06 solved

--- starter/problems/06-add-to-twelve.js
+++ starter/problems/06-add-to-twelve.js
@@ -3,17 +3,22 @@ Write a recursive function called `addToTwelve` that will return true if there
 are two adjacent numbers in the input array that can sum up to 12. Otherwise,
 return false.
 
-Examples:
-
-addToTwelve([1, 3, 4, 7, 5]); // true
-addToTwelve([1, 3, 4, 7, 6]); // false
-addToTwelve([1, 11, 4, 7, 6]); // true
-addToTwelve([1, 12, 4, 7, 6]); // false
-addToTwelve([1]); // false
 ***********************************************************************/
+let addToTwelve = arr => {
+  if (arr.length <= 1) return false; // base case
 
-// your code here
+  if (arr[0] + arr[1] !== 12) arr.splice(0, 1);
+  
+  else return true;
 
+  return addToTwelve(arr)
+}
+
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+console.log(addToTwelve([1])); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
