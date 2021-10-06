79e01963a32d4f2d7d8a39f4283654a9dcb04845
Katerina Kreibich <87352324+Code-rina@users.noreply.github.com>
2021-10-06 08:06:19 -0700

    sumArrNotes

--- starter/problems/02-sum-array.js
+++ starter/problems/02-sum-array.js
@@ -24,6 +24,12 @@ const sumArray = arr => {
   
 }
 
+//breakdown
+// [] => 0 
+// 3 + sumArray([]) => 3 + 0 = 3
+// 2 + sumArray([3]) => 2 + 3 = 5
+// 1 + sumArray([2,3]) => 1 + 5 = 6
+
 console.log(sumArray([1, 2, 3])); //  6
 console.log(sumArray([0, 1, -3])); //  -2
 console.log(sumArray([1, 2, 3, 4, 5])); // 15
